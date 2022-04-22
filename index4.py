17757a88ee9bece031c1a1ea66405584437ac7d1
stefannysferreira <102430092+stefannysferreira@users.noreply.github.com>
2022-04-22 17:14:12 -0300

    Add files via upload

---
+++ index4.py
@@ -0,0 +1,10 @@
+nome = input("Digite seu nome: ")
+
+dia = input("Digite o dia em que nasceu: ")
+
+mês = input("Digite o mês em que nasceu: ")
+
+ano = input("Digite o ano em que nasceu: ")
+
+texto = (f"{nome} nasceu no dia: {dia}/{mês}/{ano}")
+print(texto.capitalize())
